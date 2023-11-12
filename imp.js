66ee00e362bef8b6dbd251359b3a0022edc0eaa3
Khalid Alanzi <khalidalanzi@Khalids-MacBook-Air.local>
2023-11-12 04:13:07 +0300

    v1

---
+++ imp.js
@@ -0,0 +1,157 @@
+let nva = document.getElementById("nva")
+let score = document.getElementById("s")
+let medst = document.getElementById("medstt")
+let scorecount = document.getElementById("score")
+let targetplace = document.getElementById("targetplace")
+scorecount.innerText = 0
+
+
+
+medst.addEventListener("click", ()=>{
+
+
+
+
+
+    x();
+
+
+})
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+
+//     let throwingImage = document.getElementById("throwingImage");
+
+//     throwingImage.addEventListener("click", ()=>{
+//         scorecount++
+
+//     })
+
+//     // Store the original position
+//     const originalPosition = {
+//       x: throwingImage.offsetLeft,
+//       y: throwingImage.offsetTop,
+//     };
+  
+//     // Function to throw the image
+//     function throwImage() {
+//       // Hide the image
+//       throwingImage.style.display = 'none';
+      
+//       // Reset the image to its original position before each throw
+//       throwingImage.style.transform = `translate(${originalPosition.x}px, ${originalPosition.y}px)`;
+  
+//       // Delay before making the image visible again
+//       setTimeout(() => {
+//         // Make the image visible again
+//         throwingImage.style.display = 'block';
+//       }, 500); // Adjust the delay as needed
+  
+//       // Set the transition after making the image visible
+//       setTimeout(() => {
+//         throwingImage.style.transition = 'transform 5s linear';
+//         throwingImage.style.transform = `translate(${getRandomNumber()}vh, -100vh)`;
+//       }, 600); // Adjust the delay as needed
+//     }
+  
+//     // Loop to repeat the throwImage function 10 times
+//     for (let i = 0; i < 10; i++) {
+//       setTimeout(throwImage, i * 3000); // Adjust the delay as needed
+//     }
+  
+    // Function to get a random number between -100 and 100
+    function getRandomNumber() {
+      return Math.random() * 100 - 100;
+    }
+//   });
+  
+
+   
+document.addEventListener('DOMContentLoaded', function () {
+    const flyingImage = document.getElementById('flyingImage');
+  
+    // Set the initial position of the image
+    let position = 0;
+  
+    function flyImage() {
+      // Move the image to the right
+      position += 2;
+      flyingImage.style.left = position + 'px';
+  
+      // If the image goes off the right side of the screen, reset its position
+      if (position > window.innerWidth) {
+        position = -flyingImage.width;
+      }
+  
+      // Request the next animation frame
+      requestAnimationFrame(flyImage);
+    }
+  
+    // Start the animation
+    flyImage();
+    flyingImage.addEventListener("click", ()=>{
+        flyingImage.src = "laug.jpeg"
+    })
+
+
+
+  });
+  
+  
+
+
+
+
+    document.addEventListener('DOMContentLoaded', function () {
+        let throwingImage = document.getElementById("throwingImage");
+        let score = 0;
+      
+        // Store the original position
+        const originalPosition = {
+          x: throwingImage.offsetLeft,
+          y: throwingImage.offsetTop,
+        };
+      
+        // Function to throw the image
+        function throwImage() {
+          // Reset the image to its original position before each throw
+          throwingImage.style.transition = 'none';
+          throwingImage.style.transform = `translate(${originalPosition.x}px, ${originalPosition.y}px)`;
+      
+          // Trigger reflow to apply the initial position
+          throwingImage.offsetHeight;
+      
+          // Set the transition for the throwing animation
+          throwingImage.style.transition = 'transform 4s linear';
+          throwingImage.style.transform = `translate(${getRandomNumber()}vh, -100vh)`;
+    }
+      
+        // Function to handle image click
+        function handleImageClick() {
+            scorecount.innerText++;
+          console.log("Score: " + score);
+          throwImage(); // Reset the image after click
+        }
+      
+        let imgbtn = document.getElementById("imgbtn")
+        // Add click event listener to the image
+        throwingImage.addEventListener('click', handleImageClick);
+      
+        // Loop to repeat the throwImage function 10 times
+        for (let i = 0; i < 10; i++) {
+          setTimeout(throwImage, i * 3000); // Adjust the delay as needed
+
+         
+        }
+
+      
+
+
+      });
+
+ 
+     
---
+++ script.js
@@ -0,0 +1,8 @@
+let easy = document.getElementById("esay")
+
+easy.addEventListener("click", ()=>{
+    easy.innerText = "منت رخمه رح تحت بس" 
+})
+
+let Medbtn = document.getElementById("med")
+let imp = document.getElementById("imp")
